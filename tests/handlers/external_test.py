af348b7ec905d85f050569737d28f4d05cc4520b
Kian-Tat Lim <ktl@slac.stanford.edu>
2024-11-16 17:22:51 -0800

    Update tests.

--- tests/handlers/external_test.py
+++ tests/handlers/external_test.py
@@ -3,6 +3,7 @@
 from __future__ import annotations
 
 import pytest
+from botocore.exceptions import NoCredentialsError
 from httpx import AsyncClient
 
 from s3proxy.config import config
@@ -11,7 +12,9 @@ from s3proxy.config import config
 @pytest.mark.asyncio
 async def test_get_index(client: AsyncClient) -> None:
     """Test ``GET /s3proxy/``."""
-    response = await client.get("/s3proxy/")
+    response = await client.get(
+        "/s3proxy/", headers={"X-Auth-Request-User": "test"}
+    )
     assert response.status_code == 200
     data = response.json()
     metadata = data["metadata"]
@@ -20,3 +23,12 @@ async def test_get_index(client: AsyncClient) -> None:
     assert isinstance(metadata["description"], str)
     assert isinstance(metadata["repository_url"], str)
     assert isinstance(metadata["documentation_url"], str)
+
+
+@pytest.mark.asyncio
+async def test_get_s3(client: AsyncClient) -> None:
+    """Test ``GET /s3proxy/s3``."""
+    with pytest.raises(NoCredentialsError):
+        await client.get(
+            "/s3proxy/s3/bucket/key", headers={"X-Auth-Request-User": "test"}
+        )
